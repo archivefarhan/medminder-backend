f1d7d7c19fbcf9052303daeaf35757b5e70fa28d
farhan <farhanj.mohamed@gmail.com>
2023-01-28 17:57:51 -0800

    added user authentication to backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_213948) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_015229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_213948) do
     t.string "date"
     t.integer "time"
     t.integer "frequency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
